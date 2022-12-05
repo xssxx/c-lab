cf6ed0f3f25799b43808550868792bac5601dc38
X <koonx6520@gmail.com>
2022-12-05 16:49:36 +0700

    Update lab1-4.c

--- Lab1/lab1-4.c
+++ Lab1/lab1-4.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
  
 int main() {
-
+    /*
+        x
+        xx
+        xxx
+        xx
+        x
+    */
     printf("x\nxx\nxxx\n xx\n  x");
+
+    // printf("Hello, world\n");
+    printf("printf(\"Hello, world\\n\");");
+
     return 0;
 }
