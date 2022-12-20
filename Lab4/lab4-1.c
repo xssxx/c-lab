6008bcee8cbf6bfccffac886c2a51db4af784cb8
X <koonx6520@gmail.com>
2022-12-20 21:03:25 +0700

    Update lab4-1.c

--- Lab4/lab4-1.c
+++ Lab4/lab4-1.c
@@ -1,45 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Do the count by your function
-// count target in n
-int count_target(int n, int target) {
-    int count = 0;
-    while (n >= 10) {
-        int digit = n % 10;
-        if (digit == target) {
-            count++;
-        }
-    n /= 10;
-    }
-    if (n == target) {
-        count++;
-    }
-
-    return count;
-}
-
 int main() {
-   char input_n[12], input_x[2];
-   fgets(input_n, 12, stdin);
-   fgets(input_x, 2, stdin);
+    char num_str[6];
+    int num;
 
-   int n;
-   int x, count;
+    fgets(num_str, 6, stdin);
+    num = atoi(num_str);
 
-   n = atoi(input_n);
-   x = atoi(input_x);
+    printf("Binary number of %d is ", num);
+    for(int i = 3; i >= 0; i--) {
+        printf("%d", (num>>i) % 2);
+    }
+    puts(".");
 
-   count = count_target(n, x);
-
-   // Display output in separate cases
-   //
-   if (count <= 0) {
-      printf("There is no \"%d\" in %d.\n", x, n);
-   } else if (count == 1) {
-      printf("There is only 1 \"%d\" in %d.\n", x, n);
-   } else {
-      printf("There are %d \"%d\"(s) in %d.\n", count, x, n);
-   }
-   return 0;
+    return 0;
 }
