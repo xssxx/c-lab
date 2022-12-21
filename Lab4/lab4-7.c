a5fa88cc2b9df5dc391f554b01e351e378ec9ff3
X <koonx6520@gmail.com>
2022-12-21 16:48:35 +0700

    Update lab4-7.c

--- Lab4/lab4-7.c
+++ Lab4/lab4-7.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    int h;
+    int h; // height
     scanf("%d", &h);
     
     for(int i = h*2-1; i >= 1; i--) {
-        int w = h-abs(h-(h*2-i));
+        int w = h-abs(h-(h*2-i)); // width
         for(int j = 0; j < w; j++) {
             printf("*");
         }
