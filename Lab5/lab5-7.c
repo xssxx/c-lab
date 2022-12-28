fe75ab1fb9bc22fe0dd2e2f7aa184e32d1c09601
X <koonx6520@gmail.com>
2022-12-28 18:13:23 +0700

    Update lab5-7.c

--- Lab5/lab5-7.c
+++ Lab5/lab5-7.c
@@ -3,12 +3,12 @@
 int main() {
     int n, i, j;
     printf("Enter the number of rows or columns: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int a[n][n];
 
     for(i = 0; i < n; i++) {
         for(j = 0; j < n; j++) {
-            a[i][j] = j+1+i;
+            a[i][j] = j + 1 + i;
         }
     }
 
