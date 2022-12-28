e6698fe1f0ae550263ce5c5b5c882ead62c3c420
X <koonx6520@gmail.com>
2022-12-28 17:35:11 +0700

    Create lab5-6.c

---
+++ Lab5/lab5-6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, a[5], b[5], c[10];
+    puts("Enter array a:");
+    for(i = 0; i < 5; i++) {
+        printf("Please enter an integer: ");
+        scanf ("%d", &a[i]);
+    }
+
+    puts("Enter array b:");
+    for(i = 0; i < 5; i++) {
+        printf("Please enter an integer: ");
+        scanf ("%d", &b[i]);
+    }
+
+    for(i = 0; i < 10; i++) {
+        c[i] = (i < 5) ? a[i] : b[i-5];
+    }
+
+    printf("Array c: ");
+    for (i = 0; i < 10; i++)
+        printf("%d ", c[i]);
+
+    printf("\n");
+    return 0;
+}
